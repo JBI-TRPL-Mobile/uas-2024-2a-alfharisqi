7dd705f07481886e885cc182d924462731d314ac
alfhaberdasi <risqiwicaksonoalfha.com>
2025-01-09 14:39:04 +0700

    membuat fil json

---
+++ learnio_app/android/app/src/main/kotlin/com/example/learnio_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learnio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/login_screens.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Sign In'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Sign Up Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
